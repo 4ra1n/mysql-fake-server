f0489bd0157254495f72f3610e62f4fcccdd35fa
4ra1n <2023503307@qq.com>
2023-06-11 20:12:42 +0800

    优化cli输出

--- fake-mysql-cli/src/main/java/me/n1ar4/fake/cli/PrintUtil.java
+++ fake-mysql-cli/src/main/java/me/n1ar4/fake/cli/PrintUtil.java
@@ -11,5 +11,16 @@ public class PrintUtil {
                 "\n　 ヽ_?　　(_／　 │／／\n　　7　　　　　　　|／\n　　＞―r￣￣~∠--|");
         System.out.println("\u001B[32;1m Fake MySQL Server Cli \u001B[0m");
         System.out.println("\u001B[32;1m Version: "+ Version.version+" \u001B[0m");
+
+        System.out.println("\u001B[32;1m############################# USAGE #############################\u001B[0m");
+        System.out.println("Deserialization user: deser_[gadget]_[params]");
+        System.out.println("\tGadget: CB|CC31|CC44|ROME|7U21|8U20|C3P0|URLDNS");
+        System.out.println("\t\tExample 1: deser_CB_calc.exe");
+        System.out.println("\t\tExample 2: deser_URLDNS_http://your-dns-log");
+        System.out.println();
+        System.out.println("Read file user: fileread_[name]");
+        System.out.println("\t\tExample 1: fileread_/etc/passwd");
+        System.out.println("\t\tExample 2: fileread_C:\\Program Files\\src.zip");
+        System.out.println("\u001B[32;1m#################################################################\u001B[0m");
     }
 }
